97b5a6d3f475bf9f969a58e5b9187543ba32b1dd
Popov Nikita <popovnikita01@gmail.com>
2025-09-23 23:19:03 +0200

    feature(backend): описан db helper для работы с подключением

---
+++ backend/app/core/db_helper.py
@@ -0,0 +1,63 @@
+from asyncio import current_task
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def init(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+)
