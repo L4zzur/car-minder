b9bbd19dc2c2a52250eb376c1e33d3cbb0eef966
Popov Nikita <popovnikita01@gmail.com>
2026-05-02 23:11:14 +0200

    feature(frontend): стили темы; основной page и layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+
+	import { client } from '$lib/api/client.gen';
+
+	client.setConfig({
+		baseUrl: ''
+	});
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher defaultMode="dark" />
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
