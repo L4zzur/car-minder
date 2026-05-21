5cf74d384930faeddb3ea48c1ee2107b6600ac19
Popov Nikita <popovnikita01@gmail.com>
2026-05-21 15:11:22 +0200

    fix(frontend): фикс scroll area

--- frontend/src/lib/components/ui/scroll-area/scroll-area.svelte
+++ frontend/src/lib/components/ui/scroll-area/scroll-area.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { ScrollArea as ScrollAreaPrimitive } from 'bits-ui';
+
 	import { cn } from '$lib/utils';
+
 	import ScrollAreaScrollbar from './scroll-area-scrollbar.svelte';
 
 	type Props = ScrollAreaPrimitive.RootProps & {
@@ -19,16 +21,12 @@
 	}: Props = $props();
 </script>
 
-<ScrollAreaPrimitive.Root
-	bind:ref
-	class={cn('relative overflow-hidden', className)}
-	{...restProps}
->
+<ScrollAreaPrimitive.Root bind:ref class={cn('relative overflow-hidden', className)} {...restProps}>
 	<ScrollAreaPrimitive.Viewport class={cn('h-full w-full rounded-[inherit]', viewportClass)}>
 		{@render children?.()}
 	</ScrollAreaPrimitive.Viewport>
 	{#if orientation === 'vertical' || orientation === 'both'}
-		<ScrollAreaScrollbar />
+		<ScrollAreaScrollbar orientation="vertical" />
 	{/if}
 	{#if orientation === 'horizontal' || orientation === 'both'}
 		<ScrollAreaScrollbar orientation="horizontal" />
